705ce6036e3e8a303d5bc53db76860c5ea75ec8f
Gabriel Barbosa <gabriel132lanna@gmail.com>
2025-11-18 20:40:52 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5365218810-20ed2",
+  "appId": "1:181563006150:web:72f5183f614e7504860bc7",
+  "apiKey": "AIzaSyDTJa6iyuT3_7S5I1t6nfbKFtsaMkiO8D8",
+  "authDomain": "studio-5365218810-20ed2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "181563006150"
+};
